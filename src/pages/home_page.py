ed33b8c3749e26a400c5a92b465fff4a71a265a9
abhijeet7py <abhijeetjathar709@gmail.com>
2026-02-10 22:52:14 +0530

    Modified tests

--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -8,4 +8,21 @@ class HomePage(BasePage):
     def click_add_rem_ele(self):
         self.click(HomePageLocators.add_rem_ele)
 
+    def click_basic_auth(self):
+        self.click(HomePageLocators.basic_auth)
+
+    def click_broken_img(self):
+        self.click(HomePageLocators.broken_img)
+
+    def click_challe_dom(self):
+        self.click(HomePageLocators.challe_dom)
+
+    def click_checkbox(self):
+        self.click(HomePageLocators.checkbox)
+
+    def click_context_menu(self):
+        self.click(HomePageLocators.context_menu)
+
+
+
 
--- src/tests/test_home_page.py
+++ src/tests/test_home_page.py
@@ -16,11 +16,57 @@ def test_a_b_test_nav(driver):
 @allure.title("Home Page Tests")
 @allure.description("Add Remove element Navigation test")
 @pytest.mark.positive
-def test_add_rem_ele(driver):
+def test_add_rem_ele_nav(driver):
     home = HomePage(driver)
     home.open(Urls.BASE_URL)
     home.click_add_rem_ele()
     assert home.driver.current_url == Urls.add_rem_ele
+
+@allure.title("Home Page Tests")
+@allure.description("Basic Auth Navigation test")
+@pytest.mark.positive
+def test_basic_auth_nav(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_basic_auth()
+    assert home.driver.current_url == Urls.basic_auth
+
+@allure.title("Home Page Tests")
+@allure.description("Broken images Navigation test")
+@pytest.mark.positive
+def test_broken_img_nav(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_broken_img()
+    assert home.driver.current_url == Urls.broken_img
+
+@allure.title("Home Page Tests")
+@allure.description("Challenging dom Navigation test")
+@pytest.mark.positive
+def test_challe_dom_nav(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_challe_dom()
+    assert home.driver.current_url == Urls.challe_dom
+
+@allure.title("Home Page Tests")
+@allure.description("Checkboxes Navigation test")
+@pytest.mark.positive
+def test_checkbox_nav(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_checkbox()
+    assert home.driver.current_url == Urls.checkbox
+
+@allure.title("Home Page Tests")
+@allure.description("Context Menu Navigation test")
+@pytest.mark.positive
+def test_context_menu_nav(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_context_menu()
+    assert home.driver.current_url == Urls.context_menu
+
 
 
 
