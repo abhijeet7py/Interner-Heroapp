b2825c4e68b1fb410bb4d526456ec8b1e9cc1d38
abhijeet7py <abhijeetjathar709@gmail.com>
2026-02-10 20:35:26 +0530

    Modified tests

--- src/tests/test_home_page.py
+++ src/tests/test_home_page.py
@@ -1,6 +1,12 @@
+import pytest
+
 from src.pages.home_page import HomePage
 from src.constants.urls import Urls
+import allure
 
+@allure.title("Home Page Tests")
+@allure.description("A/B Navigation test")
+@pytest.mark.positive
 def test_a_b_test_nav(driver):
     home = HomePage(driver)
     home.open(Urls.BASE_URL)
