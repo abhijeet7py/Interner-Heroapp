92ee15ceb9b43bb638bfaa0fdade868a21f926be
abhijeet7py <abhijeetjathar709@gmail.com>
2026-02-10 22:39:31 +0530

    Modified tests

--- src/constants/urls.py
+++ src/constants/urls.py
@@ -1,7 +1,56 @@
+from selenium.webdriver.common.by import By
+
+
 class Urls:
     BASE_URL = "https://the-internet.herokuapp.com/"
 
     a_b_testing = f"{BASE_URL}abtest"
     add_rem_ele = f"{BASE_URL}add_remove_elements/"
+    basic_auth = f"{BASE_URL}basic_auth"
+    broken_img = f"{BASE_URL}broken_images"
+    challe_dom = f"{BASE_URL}challenging_dom"
+    checkbox = f"{BASE_URL}checkboxes"
+    context_menu = f"{BASE_URL}context_menu"
+    digest_auth = f"{BASE_URL}digest_auth"
+    disappear_elem = f"{BASE_URL}disappearing_elements"
+    drag_n_drop = f"{BASE_URL}drag_and_drop"
+    dropdown = f"{BASE_URL}dropdown"
+    dyn_content = f"{BASE_URL}dynamic_content"
+    dyn_controls = f"{BASE_URL}dynamic_controls"
+    dyn_loading = f"{BASE_URL}dynamic_loading"
+    entry_ad = f"{BASE_URL}entry_ad"
+    exit_int = f"{BASE_URL}exit_intent"
+    file_download = f"{BASE_URL}download"
+    file_upload = f"{BASE_URL}upload"
+    float_menu = f"{BASE_URL}floating_menu"
+    forgot_password = f"{BASE_URL}forgot_password"
+    form_auth = f"{BASE_URL}login"
+    frames = f"{BASE_URL}frames"
+    geolocation = f"{BASE_URL}geolocation"
+    hor_slider = f"{BASE_URL}horizontal_slider"
+    hovers = f"{BASE_URL}hovers"
+    inf_scroll = f"{BASE_URL}infinite_scroll"
+    inputs = f"{BASE_URL}inputs"
+    jquery = f"{BASE_URL}jqueryui/menu"
+    js_alert = f"{BASE_URL}javascript_alerts"
+    js_error = f"{BASE_URL}javascript_error"
+    key_presses = f"{BASE_URL}key_presses"
+    dom = f"{BASE_URL}large"
+    mul_window = f"{BASE_URL}windows"
+    nes_frames = f"{BASE_URL}nested_frames"
+    notifications = f"{BASE_URL}notification_message_rendered"
+    redirect = f"{BASE_URL}redirector"
+    sec_file_down = f"{BASE_URL}download_secure"
+    shadow_dom = f"{BASE_URL}shadowdom"
+    shift_content = f"{BASE_URL}shifting_content"
+    slow_resc = f"{BASE_URL}slow"
+    sort = f"{BASE_URL}tables"
+    status_codes = f"{BASE_URL}status_codes"
+    typos = f"{BASE_URL}typos"
+    wysiwyg_editor = f"{BASE_URL}tinymce"
+
+
+
+
 
 
--- src/tests/test_home_page.py
+++ src/tests/test_home_page.py
@@ -13,6 +13,15 @@ def test_a_b_test_nav(driver):
     home.click_a_b_test()
     assert home.driver.current_url == Urls.a_b_testing
 
+@allure.title("Home Page Tests")
+@allure.description("Add Remove element Navigation test")
+@pytest.mark.positive
+def test_add_rem_ele(driver):
+    home = HomePage(driver)
+    home.open(Urls.BASE_URL)
+    home.click_add_rem_ele()
+    assert home.driver.current_url == Urls.add_rem_ele
+
 
 
 
